9c301b3da51de253e290347c11b76b97fb74f999
Mahesh <maheshsekar04@gmail.com>
2023-05-04 16:17:53 +0000

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 8080;
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.send('Hello World from IBM Cloud Essentials!');
+  res.send('<head> <title>NodeJS App</title> </head><h1 align="center">Azure Multi-App Deployment</h1> \n <body background="https://images.pexels.com/photos/237272/pexels-photo-237272.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=1500" > ' );
 })
 
 app.listen(port, host);
